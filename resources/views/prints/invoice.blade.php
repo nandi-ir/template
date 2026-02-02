4662844f3ab1a799107583d222532c191b5494c1
nandi-ir <nandiirawan327@gmail.com>
2026-02-02 09:07:07 +0700

    change invoice desc

--- resources/views/prints/invoice.blade.php
+++ resources/views/prints/invoice.blade.php
@@ -290,7 +290,7 @@
                                 </div>
                                 <div class="organization">
                                     <p class="organization-name">Yayasan Inisiatif Zakat Indonesia</p>
-                                    <p class="organization-detail">LAZNAS SK Kemenag RI No. 950 Tahun 2020</p>
+                                    <p class="organization-detail">LAZNAS SK Kemenag RI No. 1754 Tahun 2025</p>
                                     <p class="organization-detail">Alamat Jl. Raya Condet No.27-G, Batu Ampar, Kramat Jati, Jakarta Timur 13520</p>
                                     <p class="organization-detail">Telp : (021) 87787325 Fax : (021) 87787603</p>
                                 </div>
@@ -436,26 +436,17 @@
                 </p>
                 <ol class="notes-list">
                     <li>
-                        Inisiatif Zakat Indonesia terdaftar sebagai lembaga penerbit Bukti
-                        Setor Zakat (BSZ) untuk pengurangan penghasilan kena pajak
-                        berdasarkan peraturan Dirjen Pajak No. PER-08/PJ/2021.
+                        Inisiatif Zakat Indonesia terdaftar sebagai lembaga penerbit Bukti Setor Zakat (BSZ) untuk pengurangan penghasilan kena pajak berdasarkan Peraturan Dirjen Pajak No. PER-22/PJ/2025
                     </li>
                     <li>
                         Inisiatif Zakat Indonesia tidak menerima segala bentuk dana yang
                         terkait dengan terorisme dan pencucian uang.
                     </li>
                     <li>
-                        Untuk memenuhi kepatuhan terhadap Syariah serta Undang - Undang
-                        No. 23 Tahun 2011 tentang Pengelolaan Zakat. data zakat yang
-                        disetorkan oleh Penyetor (Muzaki) telah sesuai dengan
-                        kriteria/syarat wajib zakat, yaitu: (1) Muslim, (2) Milik
-                        Sempurna, (3) Cukup Nisab, (4) Cukup Haul, (5) Bersumber dari dana
-                        yang halal.
+                        Untuk memenuhi kepatuhan terhadap Syariah serta Undang-Undang No. 23 Tahun 2011 tentang Pengelolaan Zakat, data zakat yang disetorkan oleh Penyetor (Muzaki) telah sesuai dengan kriteria/syarat wajib zakat, yaitu: (1) Muslim, (2) Milik Sempurna, (3) Cukup Nisab, (4) Cukup Haul, dan (5) Bersumber dari dana yang halal.
                     </li>
                     <li>
-                        Transkasi Zakat dapat dikreditkan sebagai pengurangan Penghasilan
-                        Kena Pajak sesuai ketentuan yang berlaku (Pasal 9 ayat 1 huruf g
-                        UU No. 36 Th.2008 tentang Pajak Penghasilan).
+                        Transaksi zakat dapat dikreditkan sebagai pengurangan Penghasilan Bruto sesuai ketentuan PMK No. 114 Tahun 2025 dan Pasal 9 ayat (1) huruf g UU No. 7 Tahun 2021 tentang Harmonisasi Peraturan Perpajakan (UU HPP).
                     </li>
                 </ol>
             </div>
